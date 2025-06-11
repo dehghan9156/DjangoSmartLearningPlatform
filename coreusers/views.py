7ac9d68c8b11202e0001e9732110cc0afda1a4b7
dehghan9156 <dehghan9156@gmail.com>
2025-06-10 21:29:41 -0700

    add chart for score user

--- coreusers/views.py
+++ coreusers/views.py
@@ -9,7 +9,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from DjangoSmartLearningPlatform.settings import OPENAI_API_KEY
 from .forms import *
 from .models import *
-import requests
+import requests,json
 from django.http import JsonResponse
 from django.conf import settings
 import openai
@@ -19,7 +19,25 @@ from django.views import View
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.utils import timezone
 from learning.models import *
+
+
 class UserPanelView(View):
-    def get(self,request):
-        exam_feedback = ExamAnswer.objects.filter(student = request.user)
-        return render(request,"coreusers/user_panel.html",{"exam_feedback":exam_feedback})
+    def get(self, request):
+        exam_feedback = ExamAnswer.objects.filter(student=request.user)
+        lst_score = []
+        lst_labels = []
+        for sc in exam_feedback:
+            lst_score.append(sc.score)
+            lst_labels.append(sc.note.title)
+        max_score = max(lst_score)
+        min_score = min(lst_score)
+        context = {
+            'labels':json.dumps(lst_labels),
+            'scores':json.dumps(lst_score)
+        }
+        return render(request, "coreusers/user_panel.html",{
+                        "exam_feedback": exam_feedback,
+                       "max_score": max_score,
+                       "min_score": min_score,
+                       "lst_labels": lst_labels,
+                       "lst_score": lst_score,**context})
